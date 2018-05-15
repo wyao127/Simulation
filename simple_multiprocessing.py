b7f16c7d2d6d36bc38354668673ce8869543e9fa
wyao127 <wyao127@gmail.com>
2018-05-14 17:16:16 -0700

    Add files via upload
    simple multiprocessing function with timeout

---
+++ simple_multiprocessing.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+from multiprocessing import Process
+import time
+
+def single_process(num):
+   cnt = 0
+   while cnt<num:
+      print(cnt)
+      cnt += 1
+      time.sleep(1)
+
+if __name__ == "__main__":
+   for i in range(5):
+      p = Process(target = single_process(i))
+      p.start()
+      p.join(10)
+
+      if p.is_alive():
+         print("running test {}...".format(i))
+         p.terminate()
+         p.join()
+   
